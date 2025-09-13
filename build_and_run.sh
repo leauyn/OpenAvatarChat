1717014b25b56d66aa5db792f0096ff161ede969
leauyn <leauyn@hotmail.com>
2025-09-14 06:23:29 +0800

    init config update after pulling from remote

--- build_and_run.sh
+++ build_and_run.sh
@@ -15,13 +15,15 @@ echo "${CONFIG_PATH}"
 docker build \
     --build-arg CONFIG_FILE=${CONFIG_PATH}  \
     -t open-avatar-chat:0.0.1 . 
-docker run --rm --gpus all -it --name open-avatar-chat \
+docker run -d --restart=unless-stopped --name open-avatar-chat \
     --network=host \
+    -e PIP_INDEX_URL="https://mirrors.aliyun.com/pypi/simple/" \
+    -e UV_INDEX_URL="https://mirrors.aliyun.com/pypi/simple/" \
     -v `pwd`/build:/root/open-avatar-chat/build \
     -v `pwd`/models:/root/open-avatar-chat/models \
     -v `pwd`/ssl_certs:/root/open-avatar-chat/ssl_certs \
     -v `pwd`/config:/root/open-avatar-chat/config \
-    -v `pwd`/models/musetalk/s3fd-619a316812/:/root/.cache/torch/hub/checkpoints/ \
+    -v `pwd`/src:/root/open-avatar-chat/src \
     -p 8282:8282 \
     open-avatar-chat:0.0.1 \
     --config ${CONFIG_PATH}
